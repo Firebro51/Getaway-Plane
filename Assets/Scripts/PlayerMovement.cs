dd8d32b33a33c859749d0d58cd7bf7c9477fed22
Craig Fisher <140781290+Firebro51@users.noreply.github.com>
2025-03-26 15:45:12 -0400

    Player movement Improved
    Also added Player tracking camera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,213 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerMovementImproved : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Buffer & Coyote jump")]
+    [SerializeField] private float bufferJumpWindow = .25f;
+    private float bufferJumpActivated = -1;
+    [SerializeField] private float coyoteJumpWindow = .5f;
+    private float coyoteJumpActivated = -1;
+
+    [Header("Knockback")]
+    [SerializeField] private float knockbackDuration = 1;
+    [SerializeField] private Vector2 knockbackPower;
+    private bool isKnocked;
+
+
+    [Header("Collision")]
+    [SerializeField] private float groundCheckDistnace;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+    private bool isAirborne;
+    private bool isWallDetected;
+
+    private float xInput;
+    private float yInput;
+
+    private bool facingRight = true;
+    private int facingDir = 1;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+
+    private void Update()
+    { 
+        UpdateAirbornStatus();
+
+        if (isKnocked)
+            return;
+
+        HandleInput();
+        HandleWallSlide();
+        HandleMovement();
+        HandleFlip();
+        HandleCollision();
+        HandleAnimations();
+    }
+
+    public void Knockback()
+    {
+        if(isKnocked)
+            return;
+
+        StartCoroutine(KnockbackRoutine());
+        anim.SetTrigger("knockback");
+        rb.linearVelocity = new Vector2(knockbackPower.x * -facingDir, knockbackPower.y);
+    }
+    private IEnumerator KnockbackRoutine()
+    {
+        isKnocked = true;
+
+        yield return new WaitForSeconds(knockbackDuration);
+
+        isKnocked = false;
+    }
+
+
+    private void UpdateAirbornStatus()
+    {
+        if (isGrounded && isAirborne)
+            HandleLanding();
+
+        if (!isGrounded && !isAirborne)
+            BecomeAirborne();
+    }
+    private void BecomeAirborne()
+    {
+        isAirborne = true;
+
+        if (rb.linearVelocity.y < 0)
+            ActivateCoyoteJump();
+    }
+
+    private void HandleLanding()
+    {
+        isAirborne = false;
+
+        AttemptBufferJump();
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            JumpButton();
+            RequestBufferJump();
+        }
+    }
+
+    #region Coyote & Buffer Jump
+
+    private void RequestBufferJump()
+    {
+        if (isAirborne)
+            bufferJumpActivated = Time.time;
+    }
+    private void AttemptBufferJump()
+    {
+        if (Time.time < bufferJumpActivated + bufferJumpWindow)
+        {
+            bufferJumpActivated = Time.time - 1;
+            Jump();
+        }
+    }
+    private void ActivateCoyoteJump() => coyoteJumpActivated = Time.time;
+    private void CancelCoyoteJump() => coyoteJumpActivated = Time.time - 1;
+
+    #endregion
+
+    private void JumpButton()
+    {
+        bool coyoteJumpAvalible = Time.time < coyoteJumpActivated + coyoteJumpWindow;
+
+        if (isGrounded || coyoteJumpAvalible)
+        {
+            Jump();
+        }
+
+        CancelCoyoteJump();
+    }
+
+
+
+    private void Jump() => rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+    private void HandleWallSlide()
+    {
+        bool canWallSlide = isWallDetected && rb.linearVelocity.y < 0;
+        float yModifer = yInput < 0 ? 1 : .05f;
+
+        if (canWallSlide == false)
+            return;
+
+
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * yModifer);
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.BoxCast(
+        transform.position, 
+        new Vector2(0.4f, 0.1f), 
+        0f, 
+        Vector2.down, 
+        groundCheckDistnace, 
+        whatIsGround
+        );
+
+        isWallDetected = Physics2D.Raycast(transform.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
+    }
+
+    private void HandleAnimations()
+    {
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("IsGrounded", isGrounded);
+        anim.SetBool("IsWallDetected", isWallDetected);
+    }
+
+    private void HandleMovement()
+    {
+        if (isWallDetected)
+            return;
+
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void HandleFlip()
+    {
+        if (xInput < 0 && facingRight || xInput > 0 && !facingRight)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistnace));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + (wallCheckDistance * facingDir), transform.position.y));
+    }
+}
